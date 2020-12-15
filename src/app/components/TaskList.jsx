c53545a5ca770a37bde92197f678257642acd02d
Florencia Berazategui Conselo <fconselo@springglobal.com>
2020-12-15 17:19:36 -0300

    Demo - Adding new tasks using the UI

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import connect from 'react-redux/lib/connect/connect';
+import { requestTaskCreation } from '../store/mutations';
 
-export const TaskList = ({tasks, name}) => (
+export const TaskList = ({tasks, name, id, createNewTask }) => (
     <div>
         <h3>
             {name}
         </h3>
         <div>
-        {tasks.map(task =>(
-            <div>{task.name}</div>
-        ))}
-    </div>
+            {tasks.map(task =>(<div key={task.id}>{task.name}</div>))}
+        </div>
+        <button onClick={()=> createNewTask(id)}>Add New</button>
     </div>
 )
 
@@ -24,4 +24,10 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export const ConnectedTaskList = connect(mapStateToProps) (TaskList);
+const mapDispatchToProps = (dispatch, id)=>({
+    createNewTask(){
+        dispatch(requestTaskCreation(id));
+    }
+});
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,40 @@
-import { createStore } from 'redux';
-import { defaultState } from '../../server/defaultState'
+import { defaultState } from '../../server/defaultState';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
+
+const sagaMiddleware = createSagaMiddleware();
+import * as mutations from './mutations';
+import * as sagas from './sagas.mock';
 
 export const store = createStore(
-    function reducer(state = defaultState, action) {
-        return state;
-    }
+    combineReducers({
+       tasks(tasks = defaultState.tasks, action){
+           switch(action.type) {
+               case mutations.CREATE_TASK:
+                    return [...tasks, {
+                        id: action.id, ///ver se para ele imprime algum ID se como
+                        name:"New Task",
+                        group: action.groupID,
+                        owner: action.ownerID,
+                        isComplete: false,
+                    }];
+           }
+           return tasks;
+       },
+       comments(comments = defaultState.comments) {
+            return comments;
+        },
+        groups(groups = defaultState.groups) {
+             return groups;
+        },
+        users(users = defaultState.users) {
+            return users;
+        }
+    }),
+    applyMiddleware(createLogger(), sagaMiddleware)
 );
+
+for (let saga in sagas) {
+    sagaMiddleware.run(sagas[saga]);
+}
